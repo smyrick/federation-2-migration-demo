93341d982671f006783c60a7d5aba3fa522118d2
Lenny Burdette <lenny@apollographql.com>
2022-02-24 14:33:24 -0800

    setup project

--- gateway/index.js
+++ gateway/index.js
@@ -6,5 +6,5 @@ const app = express();
 const gateway = new ApolloGateway();
 const server = new ApolloServer({ gateway });
 await server.start();
-server.applyMiddleware({ app });
-app.listen(4000).then(() => console.log(`Gateway running`));
+server.applyMiddleware({ app, path: "/" });
+app.listen(4000, () => console.log(`Gateway running`));
